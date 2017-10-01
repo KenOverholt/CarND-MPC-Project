2b01d237c87ae51b9b1adcd5ab94ee1d2b2c1ee6
Ken <kroverholt@gmail.com>
2017-10-01 01:29:36 +0000

    adjust cost weight

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -52,7 +52,7 @@ class FG_eval {
    for (size_t i = 0; i < N; i++)
    {
     //fg[0] += 2000*CppAD::pow(vars[cte_start + i], 2);
-    fg[0] += 2000*CppAD::pow(vars[cte_start + i] - ref_cte, 2);
+    fg[0] += 1000*CppAD::pow(vars[cte_start + i] - ref_cte, 2);
     //fg[0] += 2000*CppAD::pow(vars[epsi_start + i], 2);
     fg[0] += 2000*CppAD::pow(vars[epsi_start + i] - ref_epsi, 2);
     fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);
